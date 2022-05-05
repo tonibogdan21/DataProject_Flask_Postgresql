2c4621631f4e0bea2e8ae409fdf94f5db2bd91fe
tonibogdan21 <tonibogdan21@yahoo.com>
2022-05-05 18:14:49 +0300

    updated

--- Insert_User.py
+++ Insert_User.py
@@ -1,4 +1,5 @@
 import psycopg2
+import bcrypt
 
 def InsertUser(users_data):
     global cursor, connection
@@ -12,6 +13,19 @@ def InsertUser(users_data):
         )
 
         cursor = connection.cursor()
+
+        # iterate through the list of tuples users_data to make sure we change all the
+        # user_password data we receive in case of multiple inserts once
+        for i in range(len(users_data)):
+            # change to tuple to list so we can modify the user_password situated at index 3 and then change the list
+            # back to tuple
+            modified_users_data = list(users_data[i])
+            password = "b'" + modified_users_data[3] + "'"
+            byte_pwd = password.encode('utf-8')
+            hashed = bcrypt.hashpw(byte_pwd, bcrypt.gensalt())
+            modified_users_data[3] = hashed
+            users_data[i] = tuple(modified_users_data)
+
 
         sql_insert_query_in_users = """ INSERT INTO users (user_email, user_first_name, user_last_name,
                                                     user_password, user_role_id) VALUES (%s,%s,%s,%s,%s) """
@@ -30,4 +44,4 @@ def InsertUser(users_data):
         connection.close()
         print("PostgreSQL connection is closed")
 
-InsertUser([('toni@yahoo.com', 'Toni', 'Paraschiv', 'tonibogdan21', 1)])
+InsertUser([('bogdan@yahoo.com', 'Toni', 'Paraschiv', 'tonibogdan21', 1), ('bogdan21@yahoo.com', 'Toni', 'Paraschiv', 'tonibogdan212121', 1)])
